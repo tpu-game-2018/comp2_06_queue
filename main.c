b6f7725b65b52a94a5a646ef604b9f4f206c795d
Takashi Imagire <imagire@gmail.com>
2018-11-05 15:17:23 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 
+struct quque_item{
+    int data;
+    quque_item *next;
+    
+};
+struct quque{
+    quque_item *first;
+};
+
 int main(int argc, char *argv[]) 
 {
     char str[7];
+    quque q;
+    q.first = nullptr;
     
     while(fgets(str, sizeof(str), stdin)){
+        int in = atoi(str);
+        
+        switch(in){
+            case 0:
+                // 表示
+                break;
+            case -1:
+                // 取り出し
+                break;
+            default:
+                // 追加
+                break;
+                    
+        }
+        
         printf("%s", str);
     }
 
