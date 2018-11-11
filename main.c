d8156bd8378c617cd1e1ab694b63fd42d8d045c2
Nao-ishigaki <43568604+Nao-ishigaki@users.noreply.github.com>
2018-11-12 00:47:59 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+	char str[100];
+	int que[100];
+	int x = 0;
+	int i = 0;
+	int j = 0;
+	for (i = 0;i<100;i++) {
+		que[i] = '\0';
+	}
 
-    return 0;
+
+	while (fgets(str, sizeof(str), stdin)) {
+		x = atoi(str);
+		if (x == 0) {
+			for (i = 0;i<j;i++) {
+				printf("%d", que[i]);
+				if (i<j - 1)
+					printf(",");
+			}
+			printf("\n");
+		}
+		else if (x == -1)
+		{
+			printf("%d\n", que[0]);
+			for (i = 0;i<j;i++)
+				que[i] = que[i + 1];
+			que[j - 1] = '\0';
+			j--;
+		}
+		else
+		{
+		que[j] = x;
+		  j++;
+		}
+	}
+	return 0;
 }
